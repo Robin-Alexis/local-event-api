1f3ee60ca71db263608406314f39be2fe8db79a8
Mathéo Bocquet <matheo.bocquet@caensup.fr>
2026-04-30 10:02:54 +0200

    add style doc scalar

--- src/routes/evaluation.routes.ts
+++ src/routes/evaluation.routes.ts
@@ -5,14 +5,18 @@ export async function evaluationRoutes(app: FastifyInstance) {
   const a = app as any
 
   // Public
-  app.get("/events/:id/evaluations", evaluationController.getEventEvaluations)
+  app.get("/events/:id/evaluations", {
+        schema: { tags: ["Événement"] }
+    }, evaluationController.getEventEvaluations)
 
   // Auth requise
   app.post("/events/:id/evaluations", {
+    schema: { tags: ["Événement"], security: [{ bearerAuth: [] }] },
     preHandler: [a.authenticate]
   }, evaluationController.createEvaluation)
 
   app.delete("/evaluations/:id", {
+    schema: { tags: ["Événement"], security: [{ bearerAuth: [] }] },
     preHandler: [a.authenticate]
   }, evaluationController.deleteEvaluation)
 }
--- src/routes/event.routes.ts
+++ src/routes/event.routes.ts
@@ -5,35 +5,47 @@ export async function eventRoutes(app: FastifyInstance) {
     const a = app as any;
 
     // Public
-    app.get("/events", eventController.getEvents);
-    app.get("/events/:id", eventController.getEventById);
+    app.get("/events", {
+        schema: { tags: ["Événement"] }
+    }, eventController.getEvents);
+    
+    app.get("/events/:id", {
+        schema: { tags: ["Événement"] }
+    }, eventController.getEventById);
 
     // Connecté
     app.post("/events", {
+        schema: { tags: ["Événement"], security: [{ bearerAuth: [] }] },
         preHandler: [a.authenticate]
     }, eventController.createEvent);
 
     app.get("/events/registered", {
-    preHandler: [a.authenticate]
+        schema: { tags: ["Événement"], security: [{ bearerAuth: [] }] },
+        preHandler: [a.authenticate]
     }, eventController.getRegisteredEvents);
 
     app.put("/events/:id", {
+        schema: { tags: ["Événement"], security: [{ bearerAuth: [] }] },
         preHandler: [a.authenticate]
     }, eventController.updateEvent);
 
     app.delete("/events/:id", {
+        schema: { tags: ["Événement"], security: [{ bearerAuth: [] }] },
         preHandler: [a.authenticate]
     }, eventController.deleteEvent);
 
     app.get("/my-events", {
+        schema: { tags: ["Événement"], security: [{ bearerAuth: [] }] },
         preHandler: [a.authenticate]
     }, eventController.getMyEvents);
 
     app.post("/events/:id/join", {
+        schema: { tags: ["Événement"], security: [{ bearerAuth: [] }] },
         preHandler: [a.authenticate]
     }, eventController.joinEvent);
 
     app.post("/events/:id/leave", {
+        schema: { tags: ["Événement"], security: [{ bearerAuth: [] }] },
         preHandler: [a.authenticate]
     }, eventController.leaveEvent);
 }
